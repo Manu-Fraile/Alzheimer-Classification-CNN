79a64f05d7f5e5ce7f659f0d8782d430b3c9181b
ilonatoikka <47354458+ilonatoikka@users.noreply.github.com>
2021-05-19 15:54:34 +0200

    added code for binary labels

--- prepare_data.py
+++ prepare_data.py
@@ -3,21 +3,28 @@ import pandas as pd
 import os
 
 # dict for label renaming
-NUM_TO_LABEL = {0.0: 0, 0.5: 1, 1.0: 2, 2.0: 3}
+MULTICLASS_LABEL = {0.0: 0, 0.5: 1, 1.0: 2, 2.0: 3}
+BINARY_LABEL = {0.0: 0, 0.5: 1, 1.0: 1, 2.0: 1}
 
 
-def one_hot_encode(label_data):
-    y = np.zeros((len(label_data), 4))
-    label_data = label_data[:, 1]
-    label_data = [NUM_TO_LABEL[num] for num in label_data]
+def one_hot_encode(label_data, label_type='binary'):
+    label_data = label_data[:, 1]  # select the labels part of the array
+
+    if label_type == 'binary':
+        y = np.zeros((len(label_data), 2))
+        label_data = [BINARY_LABEL[num] for num in label_data]
+    else:
+        y = np.zeros((len(label_data), 4))
+        label_data = [MULTICLASS_LABEL[num] for num in label_data]
+
     for i in range(len(label_data)):
         y[i, label_data[i]] = 1
     return y
 
 
-def prepare_labels(file):
+def prepare_labels(file, label_type='binary'):
     raw_label = np.load(file, allow_pickle=True)
-    one_hot_label = one_hot_encode(raw_label)
+    one_hot_label = one_hot_encode(raw_label, label_type)
     return one_hot_label
 
 
@@ -33,7 +40,7 @@ def prepare_mri(path):
 
 
 if __name__ == '__main__':
-    # get labels
+    # get labels, they will be binary by default, if want multiclass, add 'multiclass' as argument
     y_train = prepare_labels('Example_data/train_labels.npy')
     y_valid = prepare_labels('Example_data/val_labels.npy')
     y_test = prepare_labels('Example_data/test_labels.npy')
@@ -44,7 +51,7 @@ if __name__ == '__main__':
     x_test = prepare_mri('Example_data/Test/')
 
     # save
-    save_folder = 'Example_data_preprocessed/'
+    save_folder = 'Example_data_preprocessed_binary/'
     np.save(save_folder + 'y_train.npy', y_train)
     np.save(save_folder + 'y_valid.npy', y_valid)
     np.save(save_folder + 'y_test.npy', y_test)
