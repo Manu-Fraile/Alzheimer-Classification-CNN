8f2d72cf923de43c77c43abbd6d6bb779c00be14
manuelfraile <manu.fra.ro@gmail.com>
2021-05-19 20:55:45 +0200

    TF-GPU working

---
+++ model_skeleton.py
@@ -0,0 +1,70 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+
+def LoadDataset():
+    x_train = np.load('./Example_data_preprocessed/x_train.npy')
+    x_valid = np.load('./Example_data_preprocessed/x_valid.npy')
+    x_test = np.load('./Example_data_preprocessed/x_test.npy')
+    y_train = np.load('./Example_data_preprocessed/y_train.npy')
+    y_valid = np.load('./Example_data_preprocessed/y_valid.npy')
+    y_test = np.load('./Example_data_preprocessed/y_test.npy')
+
+    return x_train, x_valid, x_test, y_train, y_valid, y_test
+
+
+def PlotAccuracy(history):
+    plt.figure(1)
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    # plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    plt.show()
+
+def PlotLoss(history):
+    plt.figure(2)
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    # plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    plt.show()
+
+if __name__ == '__main__':
+
+    device_name = tf.test.gpu_device_name()
+    if device_name != '/device:GPU:0':
+        raise SystemError('GPU device not found')
+    print('Found GPU at: {}'.format(device_name))
+
+    x_train, x_valid, x_test, y_train, y_valid, y_test = LoadDataset()
+
+    model = tf.keras.applications.DenseNet121(weights=None, include_top=False,
+                                              input_shape=(176, 208, 1), pooling='avg')
+
+    out = layers.Dense(4, activation='softmax')(model.output)
+
+    full_model = models.Model(inputs=model.input, outputs=out)
+
+    opt = tf.keras.optimizers.SGD(learning_rate=0.01,
+                                  momentum=0.9,
+                                  nesterov=True)
+
+    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
+
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=6, verbose=0,
+                                                  mode='auto', baseline=None, restore_best_weights=False)
+
+    full_model.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
+
+    history = full_model.fit(x=x_train, y=y_train, batch_size=64, epochs=20, verbose=1, callbacks=early_stop,
+                             validation_data=(x_valid, y_valid), shuffle=True, class_weight=None, sample_weight=None,
+                             initial_epoch=0, steps_per_epoch=None, validation_steps=None, validation_batch_size=None,
+                             validation_freq=1, max_queue_size=10, workers=1, use_multiprocessing=False)
+
+    PlotAccuracy(history)
+    PlotLoss(history)
