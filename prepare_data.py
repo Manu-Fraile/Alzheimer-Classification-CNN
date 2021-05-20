b67d8a806e2e45e6db19eb750863bb1545a76e4c
ilonatoikka <47354458+ilonatoikka@users.noreply.github.com>
2021-05-20 12:21:08 +0200

    prepared data for the axial_full dataset

--- prepare_data.py
+++ prepare_data.py
@@ -8,8 +8,6 @@ BINARY_LABEL = {0.0: 0, 0.5: 1, 1.0: 1, 2.0: 1}
 
 
 def one_hot_encode(label_data, label_type='binary'):
-    label_data = label_data[:, 1]  # select the labels part of the array
-
     if label_type == 'binary':
         y = np.zeros((len(label_data), 2))
         label_data = [BINARY_LABEL[num] for num in label_data]
@@ -24,7 +22,9 @@ def one_hot_encode(label_data, label_type='binary'):
 
 def prepare_labels(file, label_type='binary'):
     raw_label = np.load(file, allow_pickle=True)
-    one_hot_label = one_hot_encode(raw_label, label_type)
+    sorted_label = pd.DataFrame(raw_label).sort_values(by=[0])  # sort to match the mri files
+    prep_label = sorted_label[1].to_numpy()  # select the labels part of the array
+    one_hot_label = one_hot_encode(prep_label, label_type)
     return one_hot_label
 
 
@@ -40,18 +40,20 @@ def prepare_mri(path):
 
 
 if __name__ == '__main__':
+    folder_path = '../DataSliced_axial_full/DataSliced/'
+
     # get labels, they will be binary by default, if want multiclass, add 'multiclass' as argument
-    y_train = prepare_labels('Example_data/train_labels.npy')
-    y_valid = prepare_labels('Example_data/val_labels.npy')
-    y_test = prepare_labels('Example_data/test_labels.npy')
+    y_train = prepare_labels(folder_path + 'train_labels.npy')
+    y_valid = prepare_labels(folder_path + 'val_labels.npy')
+    y_test = prepare_labels(folder_path + 'test_labels.npy')
 
     # get features aka mri slices
-    x_train = prepare_mri('Example_data/Train/')
-    x_valid = prepare_mri('Example_data/Val/')
-    x_test = prepare_mri('Example_data/Test/')
+    x_train = prepare_mri(folder_path + 'Train/')
+    x_valid = prepare_mri(folder_path + 'Val/')
+    x_test = prepare_mri(folder_path + 'Test/')
 
     # save
-    save_folder = 'Example_data_preprocessed_binary/'
+    save_folder = 'axial_full_binary_data/'
     np.save(save_folder + 'y_train.npy', y_train)
     np.save(save_folder + 'y_valid.npy', y_valid)
     np.save(save_folder + 'y_test.npy', y_test)
