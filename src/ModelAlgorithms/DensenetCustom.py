8fd23b18d58a6429183360d42b41980d76a48793
manuelfraile <manu.fra.ro@gmail.com>
2021-05-23 20:02:54 +0200

    Add train for DensenetCustom

--- src/ModelAlgorithms/DensenetCustom.py
+++ src/ModelAlgorithms/DensenetCustom.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, backend, models
+import tensorflow.python.keras.losses as tfloss
+import tensorflow_addons as tfa
 
 
 # current default init values are the ones for Densenet121
 class DensenetCustom:
-    def __init__(self, gr=32, eps=1.001e-5, cf=0.5, shape=(224, 224, 1), dense_blocks=None, classes=2):
+    def __init__(self, data, saveModel, modelRoute='', gr=32, eps=1.001e-5, cf=0.5, shape=(224, 224, 1), dense_blocks=None, classes=2):
         self.eps = eps
         self.cf = cf    # compression factor
         self.shape = shape
@@ -19,6 +21,10 @@ class DensenetCustom:
             self.dense_blocks = dense_blocks
 
         self.model = self.BuildModel()
+
+        self.x_train, self.x_valid, self.x_test, self.y_train, self.y_valid, self.y_test = data
+        self.save = saveModel
+        self.modelRoute = modelRoute
 
     def BuildModel(self):
         # model = models.Sequential()
@@ -84,3 +90,39 @@ class DensenetCustom:
             x = layers.Concatenate(axis=3)([x, xi])
 
         return x
+
+    def Train(self):
+
+        full_model = self.model
+
+        opt = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+        opt_decay = tfa.optimizers.SGDW(weight_decay=0.0008, learning_rate=0.01, momentum=0.9, nesterov=True)
+
+        # loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
+        loss = tfloss.BinaryCrossentropy(from_logits=False)
+
+        early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=6, verbose=0,
+                                                      mode='auto', baseline=None, restore_best_weights=False)
+        stop_at_valacc1 = tf.keras.callbacks.EarlyStopping(monitor='val_acc', baseline=1.0, patience=0)
+
+        if self.save:
+            mcp_save = tf.keras.callbacks.ModelCheckpoint('../..' + self.modelRoute + 'trained_D121.h5',
+                                                          save_best_only=True,
+                                                          monitor='val_loss', mode='min')
+        else:
+            mcp_save = None
+
+        full_model.compile(optimizer=opt_decay, loss=loss, metrics=['accuracy'])
+
+        class_weights = {0: len(self.y_train[self.y_train[:, 1] == 1]) / len(self.y_train),
+                         1: len(self.y_train[self.y_train[:, 0] == 1]) / len(self.y_train)}
+
+        history = full_model.fit(x=self.x_train, y=self.y_train, batch_size=32, epochs=20, verbose=1,
+                                 callbacks=mcp_save,
+                                 validation_data=(self.x_valid, self.y_valid), shuffle=True, class_weight=None,
+                                 sample_weight=None,
+                                 initial_epoch=0, steps_per_epoch=None, validation_steps=None,
+                                 validation_batch_size=None,
+                                 validation_freq=1, max_queue_size=10, workers=1, use_multiprocessing=False)
+
+        return full_model, history
