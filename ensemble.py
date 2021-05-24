57752cb46958f2d0e5769c9bf8b3ad65d81aa829
ilonatoikka <47354458+ilonatoikka@users.noreply.github.com>
2021-05-24 20:42:07 +0200

    function for the ensemble

---
+++ ensemble.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from scipy.stats import mode
+from tensorflow.keras.models import load_model
+
+
+# models is a list of model objects
+def ensemble_prediction(models, x_test, y_test):
+    # collect predictions from all models
+    labels = []
+    for model in models:
+        preds = model.predict(x_test)
+        label_preds = np.argmax(preds, axis=1)
+        labels.append(label_preds)
+    # find the majority vote
+    all_labels = np.stack(labels, axis=1)
+    majority_vote = mode(all_labels, axis=1)[0].reshape(len(all_labels))
+    acc = np.sum(y_test[:, 1] == majority_vote) / len(y_test)
+    print(f'Ensemble accuracy: {acc}')
+    return majority_vote
