fa2ceb1a04cdcc32319a62d11f038a46cb0e95dd
ilonatoikka <47354458+ilonatoikka@users.noreply.github.com>
2021-05-24 20:29:31 +0200

    update eval_model
    now it also prints the precision, recall and f1 score

--- main.py
+++ main.py
@@ -92,10 +92,40 @@ def LoadModel(file):
     return model
 
 
-def EvaluateModel(model, x_test, y_test):
+def EvaluateModel(model, x_test, y_test, nclasses):
     test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1)
-    print('+++++   BEST MODEL   +++++')
-    print('The test loss: ' + str(test_loss) + '\nThe test accuracy: ' + str(test_acc))
+    # print('+++++   BEST MODEL   +++++')
+    # print('The test loss: ' + str(test_loss) + '\nThe test accuracy: ' + str(test_acc))
+    preds = model.predict(x_test)
+    label_preds = np.argmax(preds, axis=1)
+
+    # for precision and rest
+    one_hot_labels = np.zeros((len(y_test), nclasses))
+    for i in range(len(y_test)):
+        one_hot_labels[i, label_preds[i]] = 1
+
+    for j in range(nclasses):
+        print(f'Eval for class {j}')
+        print(f'Accuracy: {test_acc}')
+        extra_metric(one_hot_labels[j], y_test[j])
+        print('')
+
+
+def extra_metric(predicted, true):
+    TP = np.abs(np.sum(predicted * true))
+    TN = np.abs(np.sum((predicted - 1) * (true - 1)))
+    FP = np.abs(np.sum(predicted * (true - 1)))
+    FN = np.abs(np.sum((predicted - 1) * true))
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    print(f'Precision: {precision}')
+    print(f'Recall:    {recall}')
+    print(f'f1-score:  {f1}')
+
+    # return precision, recall, f1
 
 
 if __name__ == "__main__":
@@ -173,4 +203,5 @@ if __name__ == "__main__":
     else:
         model = LoadModel(modelRoute)
 
-    EvaluateModel(model, x_test, y_test)
+    # nclasses set to 2 by default
+    EvaluateModel(model, x_test, y_test, 2)
